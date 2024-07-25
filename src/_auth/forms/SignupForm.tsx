bddba9e486163b9e0cf1737c309ce211534425f0
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-07-25 15:47:28 +0530

    this is a dev commit

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -31,9 +31,14 @@ const SignupForm = () => {
 
     return (
         <div className="max-w-md mx-auto mt-8">
-            <div className="flex justify-center mb-8">
-                <img src="/assets/images/logo.svg" alt="logo" className="w-32 h-auto"/>
+            <div className="flex flex-col items-center mb-8">
+                <img src="/assets/images/logo.svg" alt="logo" className="w-32 h-auto" />
+                <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+                <p className="text-light-3 small-medium md:base-regular mt-4 text-center">
+                    To use SneakPeek enter your account details
+                </p>
             </div>
+
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
                     <FormField
